e6bfe252d29cbdcf205bb9520358e933e3507cf3
Marco <759374+mhelmich@users.noreply.github.com>
2022-02-05 09:42:27 -0800

    update description (#2)

--- cmd/root.go
+++ cmd/root.go
@@ -13,16 +13,9 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "keycloak",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "keycloak is a tool that enables a gitops approach for secrets.",
+	Long: `keycloak is a tool that enables a gitops approach for secrets.
+	This tool takes in a private key, a secrets file, decrypts the secrets using the private key and starts a child process with the secrets in the environment.`,
 }
 
 // execute adds all child commands to the root command and sets flags appropriately.
